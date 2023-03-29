e4c144a2b51b4dea7bd01da253addc48ee96bb08
zerohertz <ohg3417@gmail.com>
2023-03-29 13:05:31 +0900

    :fire: Update: Boxgen

--- models/post_processing/boxgen/const_boxgen.pyx
+++ models/post_processing/boxgen/const_boxgen.pyx
@@ -25,11 +25,11 @@ cdef np.ndarray[np.float32_t, ndim=2] _boxgen(np.ndarray[np.int32_t, ndim=2] lab
     cdef np.ndarray[np.int32_t, ndim=2] points = np.full((H*W, 3), label_num + 1, dtype=np.int32)
     cdef np.ndarray[np.int32_t, ndim=2] points_New = np.full((H*W, 2), label_num + 1, dtype=np.int32)
     cdef np.ndarray[np.int32_t, ndim=1] points_idx = np.zeros(H*W, dtype=np.int32)
-    cdef np.ndarray[np.float32_t, ndim=1] pos = np.zeros(2, dtype=np.float32)
-    cdef np.ndarray[np.float32_t, ndim=1] length = np.zeros(2, dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=2] pos = np.zeros((0, 2), dtype=np.float32)
+    cdef np.ndarray[np.float32_t, ndim=2] length = np.zeros((0, 2), dtype=np.float32)
     cdef np.ndarray[np.float32_t, ndim=3] bboxes = np.zeros((0, 4, 2), dtype=np.float32)
     cdef int i, j, tmp, idx
-    cdef float deg
+    cdef float tmpdeg, deg
     cdef tuple pos_t, length_t
     
     for i in prange(H, nogil=True):
@@ -54,6 +54,7 @@ cdef np.ndarray[np.float32_t, ndim=2] _boxgen(np.ndarray[np.int32_t, ndim=2] lab
     points_New = points[points_idx][:, 1:3]
     
     tmp = 0
+    deg = 0
     for i in range(1, label_num):
         idx = int(area[i])
         if area[i] < min_area:
@@ -66,16 +67,23 @@ cdef np.ndarray[np.float32_t, ndim=2] _boxgen(np.ndarray[np.int32_t, ndim=2] lab
             label[inds[i]] = 0
             continue
         
-        pos_t, length_t, deg = cv2.minAreaRect(points_New[tmp:tmp+idx][:, ::-1])
+        pos_t, length_t, tmpdeg = cv2.minAreaRect(points_New[tmp:tmp+idx][:, ::-1])
         if length_t[0] < 1.0 or length_t[1] < 1.0:
             continue
-
-        pos, length = np.array(pos_t, dtype=np.float32), np.array(length_t, dtype=np.float32)
-        pos += pos_const
-        length += len_const
-        pos, length = pos*scale, length*scale
-        bboxes = np.append(bboxes, cv2.boxPoints((pos, length, deg)).astype('float32').reshape(1, 4, 2).round(2), axis=0)
+        
+        pos = np.append(pos, np.array(pos_t, dtype=np.float32).reshape(1, 2) + pos_const, axis=0)
+        if 45 < tmpdeg <= 135:
+            deg += tmpdeg - 90
+            length = np.append(length, np.array((length_t[1], length_t[0]), dtype=np.float32).reshape(1, 2) + len_const, axis=0)
+        elif -45 <= tmpdeg <= 45:
+            deg += tmpdeg
+            length = np.append(length, np.array(length_t, dtype=np.float32).reshape(1, 2) + len_const, axis=0)
         tmp += int(area[i])
+    
+    j = len(pos)
+    deg = deg / j
+    for i in range(j):
+        bboxes = np.append(bboxes, cv2.boxPoints((pos[i]*scale, length[i]*scale, deg)).astype('float32').reshape(1, 4, 2).round(2), axis=0)
     return bboxes
 
 def boxgen(label, score, label_num, min_area, min_score, scale, pos_const, len_const):
